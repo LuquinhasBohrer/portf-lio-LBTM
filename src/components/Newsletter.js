3b30c64eb64e1fb09c391184751ac1a1b618e4d5
Lucas Bohrer <bohrer.welter.lucas@gmail.com>
2023-04-21 16:43:49 -0300

    alteração na newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,39 +2,19 @@ import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
 
 export const Newsletter = ({ status, message, onValidated }) => {
-  const [email, setEmail] = useState('');
-
-  useEffect(() => {
-    if (status === 'success') clearFields();
-  }, [status])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    email &&
-    email.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email
-    })
-  }
-
-  const clearFields = () => {
-    setEmail('');
-  }
-
   return (
       <Col lg={12}>
         <div className="newsletter-bx wow slideInUp">
           <Row>
             <Col lg={12} md={6} xl={5}>
-              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
+              <h3>Inscreva-se em nossa Newsletter<br></br> & E não perca os novos updates!</h3>
             </Col>
             <Col md={6} xl={7}>
-              <form onSubmit={handleSubmit}>
+            <form action="https://api.sheetmonkey.io/form/6sVMu3vYF8xKAtDwxy5xB6" required method="post">
                 <div className="new-email-bx">
-                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                <label> </label>
+                <input type="hidden" name="Enviado em;" value="x-sheetmonkey-current-date-time" />
+                  <input type="email" name="Newsletter" required placeholder="Email Address" />
                   <button type="submit">Submit</button>
                 </div>
               </form>
